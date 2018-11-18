fc0167036c2eb2186bb3f444d13739012ee9e2bd
mariusz <mariusz.waclawek.swe@gmail.com>
2018-11-18 15:14:39 +0100

    moved the debug draw functionality from the application into the debugdraw class

--- code/utils/DebugDraw/DebugDraw.cpp
+++ code/utils/DebugDraw/DebugDraw.cpp
@@ -8,6 +8,17 @@
 #include "Mesh.h"
 #include "ShaderManager.h"
 #include "Render.h"
+#include "CameraManager.h"
+#include "Camera.h"
+#include "Texture.h"
+#include "BoundingBoxSystem.h"
+#include "PointSystem.h"
+#include "LineSystem.h"
+#include "FrameBuffer.h"
+#include "Frustum.h"
+#include "Scene.h"
+#include "RigidBody.h"
+
 using namespace mwm;
 
 DebugDraw::DebugDraw()
@@ -66,7 +77,7 @@ void DebugDraw::DrawShapeAtPos(const char* shapeName, const Vector3& pos)
 	Object* shape = DebugDraw::Instance()->debugShapes[shapeName];
 	shape->SetPosition(pos);
 	shape->SetScale(Vector3(0.5f, 0.5f, 0.5f));
-	shape->node.UpdateNodeTransform(Node());
+	shape->node.UpdateNode(Node());
 	Render::Instance()->drawSingle(shape, *View**Projection, ShaderManager::Instance()->GetCurrentShaderID());
 }
 
@@ -89,7 +100,6 @@ void DebugDraw::DrawLine(const Vector3& normal, const Vector3& position, float w
 	}
 
 	model = Matrix4::scale(Vector3(length, length, length))*model;
-	
 	
 	GLuint prevShader = ShaderManager::Instance()->GetCurrentShaderID();
 	GLuint wireframeShader = GraphicsStorage::shaderIDs["wireframe"];
@@ -180,10 +190,12 @@ void DebugDraw::DrawPoint(const Vector3& position, float size)
 	ShaderManager::Instance()->SetCurrentShader(prevShader);
 }
 
-void DebugDraw::DrawCrossHair(int windowWidth, int windowHeight, const Vector3F& color)
+void DebugDraw::DrawCrossHair(const Vector3F& color)
 {
 	double scale = 20.0;
 	double offset = scale / 2.0;
+	double windowWidth = (double)CameraManager::Instance()->GetCurrentCamera()->windowWidth;
+	double windowHeight = (double)CameraManager::Instance()->GetCurrentCamera()->windowHeight;
 	double x = windowWidth / 2.0;
 	double y = windowHeight / 2.0;
 	Matrix4 model1 = Matrix4::translate(x, y + offset, 0.0);
@@ -195,7 +207,7 @@ void DebugDraw::DrawCrossHair(int windowWidth, int windowHeight, const Vector3F&
 	model1 = scaleM * model1;
 	model2 = scaleM * model2;
 	Matrix4 view = Matrix4(1);
-	Matrix4 proj = Matrix4::orthographicTopToBottom(-1.0, 2000.0, 0.0, (double)windowWidth, (double)windowHeight, 0.0);
+	Matrix4 proj = Matrix4::orthographicTopToBottom(-1.0, 2000.0, 0.0, windowWidth, windowHeight, 0.0);
 	line.mat->color = color;
 
 	GLuint prevShader = ShaderManager::Instance()->GetCurrentShaderID();
@@ -215,7 +227,7 @@ void DebugDraw::DrawQuad()
 	glDrawElements(GL_TRIANGLES, plane.mesh->indicesSize, GL_UNSIGNED_SHORT, (void*)0); // mode, count, type, element array buffer offset
 }
 
-void DebugDraw::DrawMap(int posX, int posY, int width, int height, unsigned int textureHandle, int windowWidth, int windowHeight)
+void DebugDraw::DrawRegion(int posX, int posY, int width, int height, const Texture* texture)
 {
 	glEnable(GL_SCISSOR_TEST);
 	glScissor(posX, posY, width, height);
@@ -224,9 +236,125 @@ void DebugDraw::DrawMap(int posX, int posY, int width, int height, unsigned int 
 	glViewport(posX, posY, width, height);
 
 	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, textureHandle);
+	glBindTexture(GL_TEXTURE_2D, texture->handle);
 	DrawQuad();
 
 	glBindTexture(GL_TEXTURE_2D, 0);
-	glViewport(0, 0, windowWidth, windowHeight);
+	glViewport(0, 0, CameraManager::Instance()->GetCurrentCamera()->windowWidth, CameraManager::Instance()->GetCurrentCamera()->windowHeight);
+}
+
+void DebugDraw::Clear()
+{
+	lineSystems.clear();
+	bbSystems.clear();
+	pointSystems.clear();
+}
+
+void DebugDraw::Init(Object* debugObject)
+{
+	BoundingBoxSystem* bbs = new BoundingBoxSystem(3000);
+	bbSystems.push_back(bbs);
+	LineSystem* ls = new LineSystem(4000);
+	lineSystems.push_back(ls);
+	PointSystem* ps = new PointSystem(7000);
+	pointSystems.push_back(ps);
+	debugObject->AddComponent(bbs);
+	debugObject->AddComponent(ls);
+	debugObject->AddComponent(ps);
+}
+
+void
+DebugDraw::DrawFastLineSystems(const FrameBuffer * fboToDrawTo, const GLenum * attachmentsToDraw, const int countOfAttachments)
+{
+	glDepthMask(GL_TRUE);
+	glEnable(GL_DEPTH_TEST);
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fboToDrawTo->handle);
+	glDrawBuffers(countOfAttachments, attachmentsToDraw);
+
+	GLuint fastLineShader = GraphicsStorage::shaderIDs["fastLine"];
+	ShaderManager::Instance()->SetCurrentShader(fastLineShader);
+	for (auto& lSystem : lineSystems)
+	{
+		lSystem->Draw(CameraManager::Instance()->ViewProjection, fastLineShader, 1.f);
+	}
+
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+	glDepthMask(GL_FALSE);
+}
+
+void
+DebugDraw::DrawFastPointSystems(const FrameBuffer * fboToDrawTo, const GLenum * attachmentsToDraw, const int countOfAttachments)
+{
+	glDepthMask(GL_TRUE);
+	glEnable(GL_DEPTH_TEST);
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fboToDrawTo->handle);
+	glDrawBuffers(countOfAttachments, attachmentsToDraw);
+
+	GLuint fastLineShader = GraphicsStorage::shaderIDs["fastLine"];
+	ShaderManager::Instance()->SetCurrentShader(fastLineShader);
+	for (auto& poSystem : pointSystems)
+	{
+		//poSystem->Update();
+		poSystem->Draw(CameraManager::Instance()->ViewProjection, fastLineShader, 5.f);
+	}
+
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+	glDepthMask(GL_FALSE);
+}
+
+void
+DebugDraw::DrawBoundingBoxes()
+{
+	glDepthMask(GL_TRUE);
+	glEnable(GL_DEPTH_TEST);
+	for (auto& bbSystem : bbSystems)
+	{
+		//the outcommented code is for case we want to render to post effect buffer so the bbs will be affected by effects
+		//FBOManager::Instance()->BindFrameBuffer(draw, FBOManager::Instance()->lightAndPostFrameBufferHandle); //we bind the lightandposteffect buffer for drawing
+		//GLenum DrawBuffers[] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1 };
+		//glDrawBuffers(2, DrawBuffers);
+
+		GLuint fastBBShader = GraphicsStorage::shaderIDs["fastBB"];
+		ShaderManager::Instance()->SetCurrentShader(fastBBShader);
+
+		for (auto& obj : Scene::Instance()->renderList)
+		{
+			if (FrustumManager::Instance()->isBoundingSphereInView(obj->node.centeredPosition, obj->radius))
+			{
+				if (RigidBody* body = obj->GetComponent<RigidBody>())
+				{
+					FastBoundingBox* fastOBB = bbSystem->GetBoundingBoxOnce();
+					fastOBB->color = Vector4F(body->obb.color, 1.f);
+					fastOBB->node.TopDownTransform = body->obb.model;
+
+					FastBoundingBox* fastAABB = bbSystem->GetBoundingBoxOnce();
+					fastAABB->color = Vector4F(body->aabb.color, 1.f);
+					fastAABB->node.TopDownTransform = body->aabb.model;
+				}
+			}
+		}
+
+		for (auto& obj : Scene::Instance()->pointLights)
+		{
+			if (FrustumManager::Instance()->isBoundingSphereInView(obj->node.centeredPosition, obj->radius))
+			{
+				if (RigidBody* body = obj->GetComponent<RigidBody>())
+				{
+					FastBoundingBox* fastOBB = bbSystem->GetBoundingBoxOnce();
+					fastOBB->color = Vector4F(body->obb.color, 1.f);
+					fastOBB->node.TopDownTransform = body->obb.model;
+
+					FastBoundingBox* fastAABB = bbSystem->GetBoundingBoxOnce();
+					fastAABB->color = Vector4F(body->aabb.color, 1.f);
+					fastAABB->node.TopDownTransform = body->aabb.model;
+				}
+			}
+		}
+		//bbSystem->Update();
+		bbSystem->Draw(CameraManager::Instance()->ViewProjection, fastBBShader);
+
+		//FBOManager::Instance()->UnbindFrameBuffer(draw);
+		
+	}
+	glDepthMask(GL_FALSE);
 }
--- code/utils/DebugDraw/DebugDraw.h
+++ code/utils/DebugDraw/DebugDraw.h
@@ -12,9 +12,15 @@ class Object;
 class Mesh;
 class Material;
 class FrameBuffer;
+class Texture;
+class LineSystem;
+class BoundingBoxSystem;
+class PointSystem;
+class FrameBuffer;
 
 class DebugDraw
 {
+	typedef unsigned int GLenum;
 public:
 	static DebugDraw* Instance();
 
@@ -24,9 +30,14 @@ public:
 	void DrawPlaneN(const mwm::Vector3& normal, const mwm::Vector3& position, const mwm::Vector3& halfExtent = mwm::Vector3(1.0, 1.0, 1.0));
 	void DrawPoint(const mwm::Vector3& position, float size = 10.f);
 	void DrawNormal(const mwm::Vector3& normal, const mwm::Vector3& position, float width = 4.f);
-	void DrawCrossHair(int windowWidth, int windowHeight, const mwm::Vector3F& color = mwm::Vector3F(1.f, 1.f, 0.f));
+	void DrawCrossHair(const mwm::Vector3F& color = mwm::Vector3F(1.f, 1.f, 0.f));
 	void DrawQuad();
-	void DrawMap(int posX, int posY, int width, int height, unsigned int textureHandle, int windowWidth, int windowHeight);
+	void DrawRegion(int posX, int posY, int width, int height, const Texture* texture);
+	void Clear();
+	void Init(Object* debugObject);
+	void DrawFastLineSystems(const FrameBuffer * fboToDrawTo, const GLenum * attachmentsToDraw, const int countOfAttachments);
+	void DrawFastPointSystems(const FrameBuffer * fboToDrawTo, const GLenum * attachmentsToDraw, const int countOfAttachments);
+	void DrawBoundingBoxes();
 	Material* debugMat;
 	
 	mwm::Matrix4* Projection;
@@ -38,6 +49,9 @@ public:
 	Plane plane;
 	Point point;
 	bool debug = false;
+	std::vector<LineSystem*> lineSystems;
+	std::vector<BoundingBoxSystem*> bbSystems;
+	std::vector<PointSystem*> pointSystems;
 private:
 	DebugDraw();
 	~DebugDraw();
